4c5e010df3bee46032cb0ebff34d0ba3857c4936
dilangestapp <nguentouoc@gmail.com>
2026-04-24 11:45:00 +0100

    Add user login activity model

---
+++ app/Models/UserLoginActivity.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserLoginActivity extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'event',
+        'ip_address',
+        'user_agent',
+        'guard',
+        'session_id',
+        'occurred_at',
+    ];
+
+    protected $casts = [
+        'occurred_at' => 'datetime',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
