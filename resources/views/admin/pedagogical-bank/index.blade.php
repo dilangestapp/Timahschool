9646d66e7f1c0f6f4ef175fd338b793767e5b844
dilangestapp <nguentouoc@gmail.com>
2026-05-05 19:05:30 +0200

    Add pedagogical bank placeholder view

---
+++ resources/views/admin/pedagogical-bank/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.admin')
+@section('title','Banque pédagogique')
+@section('page_title','Banque pédagogique')
+@section('content')
+<div class="admin-compact-page">
+<section class="admin-list-panel">
+<h2>Banque pédagogique</h2>
+<p>Page en cours de stabilisation.</p>
+</section>
+</div>
+@endsection
