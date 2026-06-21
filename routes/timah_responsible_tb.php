41ee16d138d3bbb7fb163a15b3281f1533c0490f
dilangestapp <nguentouoc@gmail.com>
2026-06-21 13:16:02 +0100

    Ajout routes TB responsables pédagogiques

--- routes/timah_responsible_tb.php
+++ routes/timah_responsible_tb.php
@@ -9,3 +9,17 @@ Route::get('/supervision/tb', function () {
 Route::get('/secretariat/dashboard', function () {
     return view('supervision.secretary-general');
 })->middleware(['auth', 'no.cache'])->name('secretariat.dashboard');
+
+Route::get('/responsable-enseignement/dashboard', function () {
+    return view('supervision.responsible-area', [
+        'requiredScope' => 'division',
+        'dashboardTitle' => 'TB Responsable type d’enseignement',
+    ]);
+})->middleware(['auth', 'no.cache'])->name('responsible.division.dashboard');
+
+Route::get('/responsable-departement/dashboard', function () {
+    return view('supervision.responsible-area', [
+        'requiredScope' => 'department',
+        'dashboardTitle' => 'TB Responsable département / filière',
+    ]);
+})->middleware(['auth', 'no.cache'])->name('responsible.department.dashboard');
