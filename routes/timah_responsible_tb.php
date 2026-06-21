19193192a8847d16b2570a1507497d4ccf6b3dc0
dilangestapp <nguentouoc@gmail.com>
2026-06-21 12:18:36 +0100

    Ajout route TB secrétaire général

--- routes/timah_responsible_tb.php
+++ routes/timah_responsible_tb.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/supervision/tb', function () {
     return view('supervision.responsible-tb');
 })->middleware(['auth', 'no.cache'])->name('supervision.tb');
+
+Route::get('/secretariat/dashboard', function () {
+    return view('supervision.secretary-general');
+})->middleware(['auth', 'no.cache'])->name('secretariat.dashboard');
